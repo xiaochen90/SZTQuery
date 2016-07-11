2f95c617cdaa37b97837eb938edf2fc1cc7f7449
xiaochen <1021690735@qq.com>
2016-07-11 18:15:53 +0800

    新项目

---
+++ SZTQuery/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace SZTQuery {
+    partial class MainForm {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtCardID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtResult.Location = new System.Drawing.Point(0, 28);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(401, 155);
+            this.txtResult.TabIndex = 2;
+            this.txtResult.Text = "回车键开始查询，如果出现异常请多尝试几次";
+            this.txtResult.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.txtResult_MouseDoubleClick);
+            // 
+            // txtCardID
+            // 
+            this.txtCardID.Location = new System.Drawing.Point(142, 2);
+            this.txtCardID.Name = "txtCardID";
+            this.txtCardID.Size = new System.Drawing.Size(97, 21);
+            this.txtCardID.TabIndex = 3;
+            this.txtCardID.Text = "294787053";
+            this.txtCardID.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtCardID_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 183);
+            this.Controls.Add(this.txtCardID);
+            this.Controls.Add(this.txtResult);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "余额查询";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtCardID;
+    }
+}
+
---
+++ SZTQuery/MainForm.cs
@@ -0,0 +1,98 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SZTQuery {
+    public partial class MainForm : Form {
+        private delegate void QueryDelegate();
+        private QueryDelegate query;
+        public MainForm() {
+            InitializeComponent();
+            this.txtResult.Text = "正在查询......";
+            query = new QueryDelegate(DoQuery);
+            query.BeginInvoke(null,null);
+        }
+
+        private void DoQuery() {
+            try {
+                string cardno = this.txtCardID.Text;
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://query.shenzhentong.com:8080/sztnet/qryCard.do");
+                httpWebRequest.ContentType = "application/x-www-form-urlencoded";
+                httpWebRequest.Method = "POST";
+                httpWebRequest.Timeout = 200000;
+                byte[] btBodys = Encoding.UTF8.GetBytes(string.Format("cardno={0}", cardno));
+                httpWebRequest.ContentLength = btBodys.Length;
+                httpWebRequest.GetRequestStream().Write(btBodys, 0, btBodys.Length);
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream(), Encoding.Default);
+                string responseContent = streamReader.ReadToEnd();
+                httpWebResponse.Close();
+                streamReader.Close();
+                httpWebRequest.Abort();
+                httpWebResponse.Close();
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(responseContent);
+                HtmlNode node = doc.DocumentNode.SelectSingleNode("//table[@class='tableact']/tr");
+                if (node != null) {
+                    StringBuilder text = new StringBuilder();
+                    int i = 0;
+                    foreach (HtmlNode tdNode in node.ChildNodes) {
+                        i++;
+                        if (i % 2 == 0) {
+                            text.AppendLine(tdNode.InnerText);
+                        }
+                        else {
+                            text.Append(tdNode.InnerText);
+                        }
+                    }
+                    AddMsg( text.ToString());
+                    string dirPath = Environment.CurrentDirectory;
+                    string filePath=Path.Combine(dirPath,"Record.txt");
+                    FileInfo file = new FileInfo(filePath);
+                    FileStream fileStream;
+                    if (!file.Exists) {
+                        fileStream = file.Create();
+                    }
+                    else {
+                        fileStream = file.Open(FileMode.Append, FileAccess.Write);
+                    }
+                    StreamWriter sw = new StreamWriter(fileStream);
+                    sw.WriteLine("--------------------------------------------");
+                    sw.WriteLine("查询时间："+DateTime.Now);
+                    sw.WriteLine(text.ToString());
+                    sw.Close();
+                    fileStream.Close();
+                }
+                else {
+                    AddMsg( "查询异常！");
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+                AddMsg(ex.ToString());
+            }
+        }
+        private void AddMsg(string msg) {
+            Invoke(new MethodInvoker(delegate() {
+                        this.txtResult.Text=msg;
+                    }));
+        }
+
+        private void txtResult_MouseDoubleClick(object sender, MouseEventArgs e) {
+            this.txtResult.Text = string.Empty;
+        }
+
+        private void txtCardID_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                query.BeginInvoke(null, null);
+            }
+        }
+    }
+}
